dd6c1179055f3bd9eebfe44edceb65b1a80ef9ba
Kira Chen <kira.chen@linux88.com>
2024-05-25 18:45:08 +0800

    feat: Add Dockerfile and Kubernetes resources for Cilium network configuration

---
+++ kubernetes/net/cilium/README.md
@@ -0,0 +1,15 @@
+## [Kubernetes Without kube-proxy](https://docs.cilium.io/en/v1.15/network/kubernetes/kubeproxy-free/)
+
+### [Socket LoadBalancer](https://cilium.io/blog/2019/08/20/cilium-16/)
+
+### XDP Load Balancing
+
+## 加密
+
+### [IPsec](https://docs.cilium.io/en/v1.15/security/network/encryption-ipsec/#ipsec-transparent-encryption)
+
+### [WireGuard](https://docs.cilium.io/en/v1.15/security/network/encryption-wireguard/)
+
+## 参考和引用
+
+- https://cilium.io/blog/2021/05/11/cni-benchmark/
---
+++ kubernetes/net/cilium/basics/vxlan-with-kubeproxy/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+readonly KIND_NAME="vxlan-with-kubeproxy"
+
+# Switch to the script's directory
+cd "$(dirname "$0")"
+
+# Prepare noCNI env
+kind create cluster --name=$KIND_NAME --image=mykindest/node:v1.28.7 --config=./kind.yaml
+
+# Remove taints
+controller_node=$(kubectl get nodes --no-headers -o custom-columns=NAME:.metadata.name | grep control-plane)
+kubectl taint nodes $controller_node node-role.kubernetes.io/control-plane:NoSchedule-
+
+# Fix https://github.com/cilium/cilium/issues/23838
+sudo chown -R root:root /opt/cni/bin
+
+# Load images
+# kind load docker-image quay.io/cilium/operator-generic:v1.15.5 --name=$KIND_NAME
+
+# Install CNI
+# helm search repo cilium/cilium --versions
+# helm show values cilium/cilium
+# https://github.com/cilium/charts
+helm repo add cilium https://helm.cilium.io >/dev/null 2>&1
+helm upgrade --install cilium cilium/cilium --namespace kube-system --create-namespace --version 1.15.5 -f cilium-values.yaml \
+  --set k8sServiceHost=$controller_node \
+  --set k8sServicePort=6443
+
+# Install necessary tools
+# for i in $(docker ps -a --format "table {{.Names}}" | grep cilium); do
+#   echo $i
+#   docker cp /usr/local/bin/cilium $i:/usr/local/bin/cilium
+#   docker cp /usr/bin/ping $i:/usr/bin/ping
+# done
+
+# Run a test pod
+# kubectl apply -f ./test-pod.yaml
+# kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
